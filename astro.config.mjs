f49e69ed99761fcb8e2b4d56e50ce2480305c592
Sagari251 <saga@iqumulus.com>
2026-07-05 21:17:30 -0500

    manual sitemap, remove broken plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
 	site: 'https://sagarikaramesh.com',
 	output: 'static',
 	adapter: vercel(),
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind()],
 });

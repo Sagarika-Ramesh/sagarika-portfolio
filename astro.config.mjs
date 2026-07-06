b7a7962f4e959d39d4a34e9c5de9696101eae4d6
Sagari251 <saga@iqumulus.com>
2026-07-05 21:07:55 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+	site: 'https://sagarikaramesh.com',
 	output: 'static',
 	adapter: vercel(),
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap()],
 });
